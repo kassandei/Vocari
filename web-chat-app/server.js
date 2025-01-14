76c8ca9395890fbfc1c8f41611544451983c7a05
kassandei <skyfox839@gmail.com>
2025-01-14 17:19:58 +0100

    feat(chat): implement chat history storage and enhance UI with login form and improved message input layout

--- web-chat-app/server.js
+++ web-chat-app/server.js
@@ -9,13 +9,18 @@ const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'src')));
 
+let chatHistory = [];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.emit('chat history', chatHistory); // Send chat history to the new user
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 
     socket.on('chat message', (msg) => {
+        chatHistory.push(msg); // Store the message in chat history
         io.emit('chat message', msg);
     });
 });
