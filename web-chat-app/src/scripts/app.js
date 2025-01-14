63fa51a32972e9a9e64cf10b05f230e5e3789f27
kassandei <skyfox839@gmail.com>
2025-01-14 21:50:34 +0100

    style(chat): adjust online users positioning and add margin to chat container

--- web-chat-app/src/scripts/app.js
+++ web-chat-app/src/scripts/app.js
@@ -141,13 +141,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-    // Move the user list to the left of the chatbox when pressed
+    // Move the user list under and to the left of the button when pressed
     toggleUsersButton.addEventListener('click', () => {
         if (onlineUsers.style.display === 'none' || onlineUsers.style.display === '') {
             onlineUsers.style.display = 'block';
-            const rect = chatContainer.getBoundingClientRect();
-            onlineUsers.style.top = `${rect.top + window.scrollY}px`;
-            onlineUsers.style.left = `${rect.left - onlineUsers.offsetWidth - 10 + window.scrollX}px`;
+            const rect = toggleUsersButton.getBoundingClientRect();
+            onlineUsers.style.top = `${rect.bottom + window.scrollY}px`;
+            onlineUsers.style.left = `${rect.left - onlineUsers.offsetWidth + window.scrollX}px`;
         } else {
             onlineUsers.style.display = 'none';
         }
