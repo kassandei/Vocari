cc835a7a403d9393d8787ed1e13ad25d4eaaf081
Kassandei <skyfox839@gmail.com>
2025-01-14 16:38:54 +0100

    refactor chat application: update message handling and improve UI structure

--- web-chat-app/server.js
+++ web-chat-app/server.js
@@ -7,27 +7,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let chatHistory = [];
-
 app.use(express.static(path.join(__dirname, 'src')));
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    // Send chat history to the newly connected user
-    socket.emit('chatHistory', chatHistory);
-
-    socket.on('sendMessage', (data) => {
-        chatHistory.push(data);
-        io.emit('receiveMessage', data);
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
 
-const PORT = process.env.PORT || 80;
-server.listen(PORT, () => {
-    console.log(`Server is running on http://vocari.me:${PORT}`);
+server.listen(80, () => {
+    console.log('listening on *:80');
 });
