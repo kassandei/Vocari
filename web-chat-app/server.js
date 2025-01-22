e60e562b9b233eac0c7af3c2584fe6058e615679
Kassandei <skyfox839@gmail.com>
2025-01-22 18:00:05 +0100

    refactor(server): remove HTTP server and related socket handling for HTTPS only

--- web-chat-app/server.js
+++ web-chat-app/server.js
@@ -1,54 +1,15 @@
 const express = require('express');
-const http = require('http');
 const https = require('https');
 const fs = require('fs');
 const socketIo = require('socket.io');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 const users = new Set();
 
 app.use(express.static(path.join(__dirname, 'src')));
 
 let chatHistory = [];
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.emit('chat history', chatHistory); // Send chat history to the new user
-
-    socket.on('check username', (username, callback) => {
-        if (users.has(username)) {
-            callback(false);
-        } else {
-            users.add(username);
-            socket.username = username;
-            callback(true);
-            io.emit('update users', Array.from(users)); // Update all clients with the new user list
-        }
-    });
-
-    socket.on('disconnect', () => {
-        if (socket.username) {
-            users.delete(socket.username);
-            io.emit('update users', Array.from(users)); // Update all clients with the new user list
-        }
-    });
-
-    socket.on('chat message', (message) => {
-        chatHistory.push(message);
-        io.emit('chat message', message);
-    });
-});
-
-// Redirect HTTP to HTTPS
-app.use((req, res, next) => {
-    if (!req.secure) {
-        return res.redirect(`https://${req.headers.host}${req.url}`);
-    }
-    next();
-});
 
 // Load SSL certificate and key
 const privateKey = fs.readFileSync(path.join(__dirname, 'vocari_me/private.key'), 'utf8');
@@ -95,8 +56,4 @@ httpsIo.on('connection', (socket) => {
 
 httpsServer.listen(443, () => {
     console.log('HTTPS server listening on port 443');
-});
-
-server.listen(80, () => {
-    console.log('HTTP server listening on port 80');
 });
