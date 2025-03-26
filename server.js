0abd465a090b95d7f0717b6c4770241605f3e1ff
kassandei <43384947+kassandei@users.noreply.github.com>
2025-03-26 12:00:43 +0100

    Revert "style: add timestamp to chat messages and display in UTC for consistency"
    This reverts commit be9a502be3d5565196fce7c74d4fa9e6362afa77.

--- server.js
+++ server.js
@@ -96,10 +96,8 @@ io.on('connection', (socket) => {
     });
 
     socket.on('chat message', (message) => {
-        const timestamp = new Date().toISOString(); // Salva l'ora in formato UTC
-        const messageWithTimestamp = { ...message, date: timestamp };
-        chatHistory.push(messageWithTimestamp); // Salva il messaggio con l'ora
-        io.emit('chat message', messageWithTimestamp); // Invia il messaggio con l'ora
+        chatHistory.push(message);
+        io.emit('chat message', message);
     });
 });
 
